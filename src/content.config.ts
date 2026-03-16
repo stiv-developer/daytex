a46dc7bbc3ee0f7480ab64ee6b9ffab2c8b30b67
stiv-dev-front <estvrak31@gmail.com>
2026-03-16 15:11:11 -0500

    First commit

--- src/content.config.ts
+++ src/content.config.ts
@@ -11,9 +11,9 @@ const blog = defineCollection({
 			title: z.string(),
 			description: z.string(),
 			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: z.optional(image()),
+			// pubDate: z.coerce.date(),
+			// updatedDate: z.coerce.date().optional(),
+			// heroImage: z.optional(image()),
 		}),
 });
 
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'offsetti-blue':'#00aeef',
+        'offsetti-dark': '#1e1e1e',
+        'offsetti-pink': '#E6007E',
+        'offsetti-yellow': '#FFED00',
+      }
+    },
+  },
+  plugins: [],
+}
